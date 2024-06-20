4a6c30ed1d0970dfc79144f2a24047c23c8efa6b
FiodarM <fyodormorozko95@gmail.com>
2024-06-20 16:00:17 +0300

    Build cython ext in run_meep.sh

--- run_meep.sh
+++ run_meep.sh
@@ -3,10 +3,13 @@ set -e
 NCO=1.5
 NCL=1
 THETA=180  # deg
-L=1000  # um
+L=100  # um
 WIDTH=3  # um
 HEIGHT=2  # um
 WAVELENGTH=1.55  # um
-RESOLUTION=1
+BANDWIDTH=0.01 # um
+RESOLUTION=4
 
-mpirun -n 4 python run_meep.py $NCO $NCL $THETA $L $WIDTH $HEIGHT $WAVELENGTH $RESOLUTION
+python setup.py build_ext --inplace
+mpirun -n 4 python run_meep.py $NCO $NCL $THETA $L $WIDTH $HEIGHT $WAVELENGTH \
+    $RESOLUTION --bandwidth $BANDWIDTH
